fe682435d0ab197b2e6b482dc1b813b3d268542a
Jchem-techy <adedejijohnson00@gmail.com>
2023-06-13 23:17:19 +0100

    avatar.js is setup to display the image stored in the photoURL

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,7 +1,7 @@
-import '/Avatar.css';
+import './Avatar.css';
 import React from 'react';
-
 export default function Avatar({ src }) {
+  console.log(src);
   return (
     <div className='avatar'>
       <img src={src} alt='' />
